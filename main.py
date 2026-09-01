5de03db2723cad9a750073b5fb0366c771d6d6ba
Josh Brandt <jcbrandt@mit.edu>
2026-09-01 17:14:07 -0400

    Upload

---
+++ main.py
@@ -0,0 +1,37 @@
+from objects import *
+import CAMEO
+import report
+
+output_file = report.New_Report()
+
+PHS_GHS_codes = read_PHS_GHS_codes()
+
+print("Importing Inventory...")
+
+all_chemicals = ChemInventory.get_all_chemicals()
+
+# Check for peroxide formers and PHS
+
+print("Checking for peroxide formers and PHS...")
+
+peroxide_formers = []
+PHS = []
+
+for chemical in all_chemicals:
+
+    if CAMEO.is_peroxide_former(chemical):
+
+        peroxide_formers.append(chemical)
+
+    if any(GHS_code in chemical.GHS_codes for GHS_code in PHS_GHS_codes):
+    
+        PHS.append(chemical)
+
+
+output_file.add_list_to_report("Peroxide Formers", peroxide_formers)
+
+output_file.add_list_to_report("Particularly Hazardous Substances", PHS)
+
+print("Compiling Report...")
+
+output_file.publish_report()
